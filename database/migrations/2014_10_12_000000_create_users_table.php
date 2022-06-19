f11fbdbaef33f7b7e4eada0955151af28d9a8625
Sibber Ahmad <sibber11@gmail.com>
2022-06-20 00:32:20 +0600

    user phone number added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -34,3 +36,4 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
