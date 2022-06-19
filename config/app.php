1c2593d181c28fa37e3bb94e9fa6b2141d3480f5
Sibber Ahmad <sibber11@gmail.com>
2022-06-20 00:30:52 +0600

    ide helper and debugbar added

--- config/app.php
+++ config/app.php
@@ -209,6 +209,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
