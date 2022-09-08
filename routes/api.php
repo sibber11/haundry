d3b1860d53c38f6aecb69eb9ea512688ee1801a2
sibber11 <sibber11@gmail.com>
2022-09-08 23:53:10 +0600

    api cleanup

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('customer_list', [CustomerController::class, 'customer_list'])->name('customer_list');
