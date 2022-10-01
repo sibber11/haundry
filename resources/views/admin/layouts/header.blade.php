39740d77c99fcf0cb3a9136a43ffe88c007e3762
sibber11 <sibber11@gmail.com>
2022-10-01 10:33:49 +0600

    logo update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -10,15 +10,15 @@
     <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown user-menu">
             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
-                    class="user-image img-circle elevation-2" alt="User Image">
+                <img src="{{asset('img/washersinn.png')}}"
+                     class="user-image img-circle elevation-2" alt="User Image">
                 <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
             </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <!-- User image -->
                 <li class="user-header bg-primary">
-                    <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
-                        class="img-circle elevation-2" alt="User Image">
+                    <img src="{{asset('img/washersinn.png')}}"
+                         class="img-circle elevation-2" alt="User Image">
                     <p>
                         {{ Auth::user()->name }}
                         <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small>
@@ -28,7 +28,7 @@
                 <li class="user-footer">
                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                     <a href="#" class="btn btn-default btn-flat float-right"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         Sign out
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
