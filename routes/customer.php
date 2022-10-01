ed99b382dee70cce21ec56f5d06ce496d51c1b32
sibber11 <sibber11@gmail.com>
2022-10-01 10:38:12 +0600

    route added for customer

--- routes/customer.php
+++ routes/customer.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('order', \App\Http\Controllers\Customer\OrderController::class);
 Route::post('send_invitation', [\App\Http\Controllers\Customer\ReferController::class, 'send_refer'])->name('send_invitation');
+
+Route::get('profile', [\App\Http\Controllers\Customer\ProfileController::class, 'index'])->name('profile');
+Route::get('voucher', [\App\Http\Controllers\Customer\ProfileController::class, 'voucher'])->name('voucher');
+Route::patch('update_profile', [\App\Http\Controllers\Customer\ProfileController::class, 'update_profile'])->name('customer.update_profile');
+Route::post('claim_voucher', [\App\Http\Controllers\Customer\ProfileController::class, 'claim_voucher'])->name('claim_voucher');
