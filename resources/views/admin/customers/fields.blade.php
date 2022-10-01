78ca088f05f100214bffeac37f829d5692c6a632
sibber11 <sibber11@gmail.com>
2022-10-01 10:30:21 +0600

    input issue fix

--- resources/views/admin/customers/fields.blade.php
+++ resources/views/admin/customers/fields.blade.php
@@ -1,34 +1,34 @@
 <!-- Name Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-12">
     {!! Form::label('name', 'Name:') !!}
     {!! Form::text('name', null, ['class' => 'form-control', 'required']) !!}
 </div>
 
 <!-- Email Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-12">
     {!! Form::label('email', 'Email:') !!}
     {!! Form::email('email', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Phone Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-12">
     {!! Form::label('phone', 'Phone:') !!}
     {!! Form::text('phone', null, ['class' => 'form-control', 'required']) !!}
 </div>
 
 <!-- Phone Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-12">
     {!! Form::label('address', 'Address:') !!}
     {!! Form::text('address', null, ['class' => 'form-control', 'required', 'placeholder' => 'add shop location here as example']) !!}
 </div>
 
 <!-- password Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-12">
     {!! Form::label('password', 'Password:') !!}
-    {!! Form::password('password', ['class' => 'form-control', 'required']) !!}
+    {!! Form::password('password', ['class' => 'form-control']) !!}
 </div>
 <!-- password Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('password_confirm', 'Retype Password:') !!}
-    {!! Form::password('password_confirm', ['class' => 'form-control', 'required']) !!}
+<div class="form-group col-sm-12">
+    {!! Form::label('password_confirmation', 'Retype Password:') !!}
+    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
 </div>
