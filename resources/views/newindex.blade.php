9e87af4ab9718d6ab58ab7654c6f788cdf81206b
sibber11 <sibber11@gmail.com>
2022-10-02 13:02:45 +0600

    asset fix

--- resources/views/newindex.blade.php
+++ resources/views/newindex.blade.php
@@ -7,8 +7,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
-    <link rel="stylesheet" href="{{mix('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+          crossorigin="anonymous"/>
 </head>
 <body class="bg-gray-200">
 @include('layouts.nav')
