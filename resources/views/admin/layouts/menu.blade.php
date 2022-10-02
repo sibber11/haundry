f97571e44d8fd6a497cef23a40697142f09070c4
sibber11 <sibber11@gmail.com>
2022-10-02 20:20:55 +0600

    admin menu icon fix

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -3,7 +3,7 @@
         <li class="nav-item">
             <a href="{{ route('admin.missions.show', auth()->user()->mission) }}"
                class="nav-link {{ Request::is('admin/missions*') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-home"></i>
                 <p>Current Mission</p>
             </a>
         </li>
@@ -12,7 +12,7 @@
 
 <li class="nav-item">
     <a href="{{ route('admin.home') }}" class="nav-link {{ Request::is('admin/home') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Home</p>
     </a>
 </li>
@@ -20,7 +20,7 @@
 <li class="nav-item">
     <a href="{{ route('admin.laundryTypes.index') }}"
        class="nav-link {{ Request::is('admin/laundry*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Laundry Types</p>
     </a>
 </li>
@@ -28,36 +28,36 @@
 <li class="nav-item">
     <a href="{{ route('admin.customers.index') }}"
        class="nav-link {{ Request::is('admin/customers*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Customers</p>
     </a>
 </li>
 
 <li class="nav-item {{ Request::is('admin/orders*') ? 'menu-open ' : '' }}">
     <a href="{{ route('admin.orders.index') }}" class="nav-link {{ Request::is('admin/orders*') ? 'active ' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Orders
-            <i class="right fas fa-angle-left"></i></p>
+            <i class="right fa fa-angle-left"></i></p>
     </a>
     <ul class="nav nav-treeview ml-2">
         <li class="nav-item">
             <a href="{{ route('admin.orders.create') }}"
                class="nav-link {{ Request::is('admin/orders/create') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-home"></i>
                 <p>Create Order</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('admin.orders.index') }}"
                class="nav-link {{ Request::is('admin/orders') && !Request::has('filter') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-home"></i>
                 <p>All</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('admin.orders.index', ['filter' => 'pickable']) }}"
                class="nav-link {{ Request::is('admin/orders') && Request::input('filter') == 'pickable' ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-home"></i>
                 <p>Pickable</p>
                 <span class="badge badge-info right">{{\App\Models\Order::pickable()->count()}}</span>
             </a>
@@ -65,7 +65,7 @@
         <li class="nav-item">
             <a href="{{ route('admin.orders.index', ['filter' => 'operable']) }}"
                class="nav-link {{ Request::is('admin/orders') && Request::input('filter') == 'operable' ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-home"></i>
                 <p>Operable</p>
                 <span class="badge badge-info right">{{\App\Models\Order::operable()->count()}}</span>
             </a>
@@ -73,7 +73,7 @@
         <li class="nav-item">
             <a href="{{ route('admin.orders.index', ['filter' => 'deliverable']) }}"
                class="nav-link {{ Request::is('admin/orders') && Request::input('filter') == 'deliverable' ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-home"></i>
                 <p>Deliverable</p>
                 <span class="badge badge-info right">{{\App\Models\Order::deliverable()->count()}}</span>
             </a>
@@ -81,7 +81,7 @@
         <li class="nav-item">
             <a href="{{ route('admin.orders.index', ['filter' => 'running']) }}"
                class="nav-link {{ Request::is('admin/orders') && Request::input('filter') == 'running' ? 'active' : '' }}">
-                <i class="nav-icon fas fa-home"></i>
+                <i class="nav-icon fa fa-home"></i>
                 <p>Running</p>
                 <span class="badge badge-info right">{{\App\Models\Order::running()->count()}}</span>
             </a>
@@ -91,28 +91,28 @@
 <li class="nav-item">
     <a href="{{ route('admin.categories.index') }}"
        class="nav-link {{ Request::is('admin/categories*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Categories</p>
     </a>
 </li>
 
 <li class="nav-item">
     <a href="{{ route('admin.services.index') }}" class="nav-link {{ Request::is('admin/service*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Services</p>
     </a>
 </li>
 
 <li class="nav-item">
     <a href="{{ route('admin.missions.index') }}" class="nav-link {{ Request::is('admin/missions*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Missions</p>
     </a>
 </li>
 
 <li class="nav-item">
     <a href="{{ route('admin.packages.index') }}" class="nav-link {{ Request::is('admin/packages*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Packages</p>
     </a>
 </li>
@@ -120,7 +120,7 @@
 <li class="nav-item">
     <a href="{{ route('admin.income', ['year' => now()->year, 'month' => now()->month]) }}"
        class="nav-link {{ Request::is('admin/income*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fa fa-home"></i>
         <p>Income</p>
     </a>
 </li>
