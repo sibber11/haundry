278f1c1f6f8a48d36605f321012d0237c847bfe0
sibber11 <sibber11@gmail.com>
2022-10-15 21:58:44 +0600

    nav style fix deadline solver deleted

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -3,13 +3,13 @@
     <li class="nav-item highlight my-1">
         <a @class(['block' => $flex]) href="{{route('orders.create')}}">Order Now</a>
     </li>
-    <li @class(['nav-item my-1', 'active' => Request::routeIs('profile')])>
+    <li @class(['nav-item my-1 border-2', 'active' => Request::routeIs('profile')])>
         <a @class(['block' => $flex, 'nav-link']) href="{{route('profile')}}">Profile</a>
     </li>
-    <li @class(['nav-item my-1', 'active' => Request::routeIs('orders.index')])>
+    <li @class(['nav-item my-1 border-2', 'active' => Request::routeIs('orders.index')])>
         <a @class(['block' => $flex, 'nav-link']) href="{{route('orders.index')}}">Orders</a>
     </li>
-    <li @class(['nav-item my-1', 'active' => Request::routeIs('voucher')])>
+    <li @class(['nav-item my-1 border-2', 'active' => Request::routeIs('voucher')])>
         <a @class(['block' => $flex, 'nav-link']) href="{{route('voucher')}}">Vouchers</a>
     </li>
     <li @class(['nav-item my-1 border-2', 'active' => Request::routeIs('referview')])>
