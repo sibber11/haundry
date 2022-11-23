f79f187f734f49c91ae0c34c69e3a2866756f9ca
sibber11 <sibber11@gmail.com>
2022-11-23 15:54:34 +0600

    contact fix

--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -4,7 +4,7 @@
     </h2>
 </div>
 <div class="grid grid-cols-1 sm:grid-cols-2 sm:bg-gradient-to-l sm:from-white sm:to-transparent sm:via-gray-200">
-    <form action="" method="post">
+    <form action="{{route('post_feedback')}}" method="post">
         @csrf
         <div class="mt-1 flex flex-col gap-2">
             <label for="contact-name ">Name: </label>
@@ -15,7 +15,7 @@
             <input type="email" name="email" id="contact-email" class="p-2 rounded">
         </div>
         <div class="mt-1 flex flex-col gap-2">
-            <label for="contact-info ">Lorem: </label>
+            <label for="contact-info ">Your Feedback: </label>
             <textarea name="info" cols="30" rows="10" id="contact-info" class="p-2 rounded"></textarea>
         </div>
         <div class="mt-2">
