8b4614970018392e54ad26b49ec05137ea5930cd
sibber11 <sibber11@gmail.com>
2022-12-08 21:11:48 +0600

    redesign

--- resources/views/customer/refer.blade.php
+++ resources/views/customer/refer.blade.php
@@ -17,14 +17,14 @@
                 </button>
             </div>
         @endif
-        <h1>Refer to get bonus.</h1>
+        <h1 class="text-2xl font-bold mb-4">Refer to get bonus.</h1>
         <div>
-            <h2>You can send the url below to someone. if they sign up using the link, you will be rewarded.</h2>
-            <h2>Your referral url</h2>
+            <p>You can send the url below to someone. if they sign up using the link, you will be rewarded.</p>
+            <div>Your referral url</div>
             <div>
                 <input type="text" contenteditable="false" value="{{$user->getReferralLink()}}"
                        class="w-72 py-1 px-2 rounded my-2 rounded-r-none border-r-2" id="refer-link">
-                <button type="button" class="rounded py-1 px-2 my-2 bg-white rounded-l-none"
+                <button type="button" class="rounded py-1 px-2 my-2 bg-white rounded-l-none" title="Copy Link"
                         onclick="navigator.clipboard.writeText(document.getElementById('refer-link').value);">
                     <i class="fa fa-copy"></i>
                 </button>
