5f83708ba825d5a4783400d79273d28b4d50dde7
sibber11 <sibber11@gmail.com>
2022-12-16 20:37:05 +0600

    firebase config fix

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -57,12 +57,12 @@
 <script>
     // Your web app's Firebase configuration
     const firebaseConfig = {
-        apiKey: {{env('FIREBASE_API_KEY')}},
-        authDomain: {{env('FIREBASE_AUTH_DOMAIN')}},
-        projectId: {{env('FIREBASE_PROJECT_ID')}},
-        storageBucket: {{env('FIREBASE_STORAGE_BUCKET')}},
-        messagingSenderId: {{env('FIREBASE_MESSAGING_SENDER_ID')}},
-        appId: {{env('FIREBASE_APP_ID')}}
+        apiKey: "{{env('FIREBASE_API_KEY')}}",
+        authDomain: "{{env('FIREBASE_AUTH_DOMAIN')}}",
+        projectId: "{{env('FIREBASE_PROJECT_ID')}}",
+        storageBucket: "{{env('FIREBASE_STORAGE_BUCKET')}}",
+        messagingSenderId: "{{env('FIREBASE_MESSAGING_SENDER_ID')}}",
+        appId: "{{env('FIREBASE_APP_ID')}}"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
