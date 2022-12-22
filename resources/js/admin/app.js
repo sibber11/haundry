dc049a859b4cb697b7698c462fa0b205ab0bb31b
sibber11 <sibber11@gmail.com>
2022-12-22 21:36:47 +0600

    firebase moved to app.js

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -24,3 +24,50 @@ files.keys().map(function (key) {
 });
 
 app.mount('#app');
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    apiKey: process.env.FIREBASE_API_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+// Initialize Firebase
+
+try {
+    firebase.initializeApp(firebaseConfig);
+    const messaging = firebase.messaging();
+} catch (e) {
+
+}
+
+
+function initFirebaseMessagingRegistration() {
+    messaging.requestPermission().then(function () {
+        return messaging.getToken()
+    }).then(function (token) {
+
+        axios.post(fcmToken, {
+            _method: "PATCH",
+            token
+        }).then(({data}) => {
+            console.log(data)
+        }).catch(({response: {data}}) => {
+            console.error(data)
+        })
+
+    }).catch(function (err) {
+        console.log(`Token Error :: ${err}`);
+    });
+}
+
+try {
+    initFirebaseMessagingRegistration();
+
+    messaging.onMessage(function ({data: {body, title}}) {
+        new Notification(title, {body});
+    });
+} catch (e) {
+
+}
