93f6d6279c1d240cb9c2fc361057f47a5cb28a8b
sibber11 <sibber11@gmail.com>
2023-02-13 21:44:06 +0600

    build fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/customer/app.js', 'resources/js/admin/app.js'],
             refresh: true,
         }),
         vue({
