f31fbc16cc873c16d5aeacd46df2ffe5bc33da63
sibber11 <sibber11@gmail.com>
2023-02-14 22:02:01 +0600

    feature icon update

--- resources/views/sections/features.blade.php
+++ resources/views/sections/features.blade.php
@@ -1,21 +1,35 @@
 <section class="py-20 bg-white">
     <div class="container max-w-6xl mx-auto">
-        <h2 class="text-4xl font-bold tracking-tight text-center">Our Features</h2>
+        <h2 class="text-4xl font-bold tracking-tight text-center" id="features">Our Features</h2>
         <p class="mt-2 text-lg text-center text-gray-600">Check out our list of awesome features below.</p>
         <div class="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
 
             <div
                 class="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl"
                 data-rounded="rounded-xl" data-rounded-max="rounded-full">
-                <div class="p-3 text-white bg-blue-500 rounded-full" data-primary="blue-500"
+                <div class="p-3 text-white bg-sky-500 rounded-full" data-primary="blue-500"
                      data-rounded="rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 " viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-                        <path d="M5 8v-3a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5"></path>
-                        <circle cx="6" cy="14" r="3"></circle>
-                        <path d="M4.5 17l-1.5 5l3 -1.5l3 1.5l-1.5 -5"></path>
+                    <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path opacity="0.4"
+                              d="M15 2V12C15 13.1 14.1 14 13 14H2V7.62C2.73 8.49 3.85003 9.03 5.09003 9C6.10003 8.98 7.01 8.59 7.69 7.94C8 7.68 8.26002 7.34999 8.46002 6.98999C8.82002 6.37999 9.02 5.65997 9 4.90997C8.97 3.73997 8.45001 2.71 7.64001 2H15Z"
+                              stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path
+                            d="M22 14V17C22 18.66 20.66 20 19 20H18C18 18.9 17.1 18 16 18C14.9 18 14 18.9 14 20H10C10 18.9 9.1 18 8 18C6.9 18 6 18.9 6 20H5C3.34 20 2 18.66 2 17V14H13C14.1 14 15 13.1 15 12V5H16.84C17.56 5 18.22 5.39001 18.58 6.01001L20.29 9H19C18.45 9 18 9.45 18 10V13C18 13.55 18.45 14 19 14H22Z"
+                            stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path
+                            d="M8 22C9.10457 22 10 21.1046 10 20C10 18.8954 9.10457 18 8 18C6.89543 18 6 18.8954 6 20C6 21.1046 6.89543 22 8 22Z"
+                            stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path
+                            d="M16 22C17.1046 22 18 21.1046 18 20C18 18.8954 17.1046 18 16 18C14.8954 18 14 18.8954 14 20C14 21.1046 14.8954 22 16 22Z"
+                            stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M22 12V14H19C18.45 14 18 13.55 18 13V10C18 9.45 18.45 9 19 9H20.29L22 12Z"
+                              stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path
+                            d="M8.9992 4.90975C9.0192 5.65975 8.81922 6.37977 8.45922 6.98977C8.25922 7.34977 7.9992 7.67978 7.6892 7.93978C7.0092 8.58978 6.09922 8.97978 5.08922 8.99978C3.84922 9.02978 2.7292 8.48977 1.9992 7.61977C1.8592 7.46977 1.7392 7.29978 1.6292 7.12978C1.2392 6.53978 1.0192 5.8398 0.999195 5.0898C0.969195 3.8298 1.52919 2.67977 2.42919 1.92977C3.10919 1.36977 3.96917 1.01978 4.90917 0.999776C5.95917 0.979776 6.91921 1.35978 7.63921 1.99978C8.44921 2.70978 8.9692 3.73975 8.9992 4.90975Z"
+                            stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                            stroke-linejoin="round"/>
+                        <path opacity="0.4" d="M3.43945 5.02979L4.44946 5.98975L6.53943 3.96973" stroke="currentColor"
+                              stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
                 <h4 class="text-xl font-medium text-gray-700">Pickup and Delivery</h4>
@@ -28,15 +42,29 @@
             <div
                 class="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl"
                 data-rounded="rounded-xl" data-rounded-max="rounded-full">
-                <div class="p-3 text-white bg-blue-500 rounded-full" data-primary="blue-500"
+                <div class="p-3 text-white bg-secondary rounded-full" data-primary="blue-500"
                      data-rounded="rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 " viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M18 8a3 3 0 0 1 0 6"></path>
-                        <path d="M10 8v11a1 1 0 0 1 -1 1h-1a1 1 0 0 1 -1 -1v-5"></path>
-                        <path
-                            d="M12 8h0l4.524 -3.77a0.9 .9 0 0 1 1.476 .692v12.156a0.9 .9 0 0 1 -1.476 .692l-4.524 -3.77h-8a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h8"></path>
+                    <svg viewBox="0 0 24 24" id="quality-4" class="w-8 h-8"
+                         data-name="Line Color"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <polygon id="secondary"
+                                 points="12 8.25 11.38 9.4 10 9.59 11 10.48 10.76 11.75 12 11.15 13.24 11.75 13 10.48 14 9.59 12.62 9.4 12 8.25"
+                                 style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 1.5;"
+                                 stroke="currentColor"
+                        ></polygon>
+                        <polyline id="primary" points="5.79 13.57 3 17.56 6.03 18.46 7.91 21 10.79 16.89"
+                                  style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 1.5;"
+                                  stroke="currentColor"
+                        ></polyline>
+                        <polyline id="primary-2" data-name="primary"
+                                  points="18.21 13.57 21 17.56 17.97 18.46 16.09 21 13.21 16.89"
+                                  style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 1.5;"
+                                  stroke="currentColor"
+                        ></polyline>
+                        <circle id="primary-3" data-name="primary" cx="12" cy="10" r="7"
+                                style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 1.5;"
+                                stroke="currentColor"
+                        ></circle>
                     </svg>
                 </div>
                 <h4 class="text-xl font-medium text-gray-700">High-Quality Cleaning Techniques</h4>
@@ -49,16 +77,18 @@
             <div
                 class="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl"
                 data-rounded="rounded-xl" data-rounded-max="rounded-full">
-                <div class="p-3 text-white bg-blue-500 rounded-full" data-primary="blue-500"
+                <div class="p-3 text-white bg-tertiary rounded-full" data-primary="blue-500"
                      data-rounded="rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 " viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <polyline points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3"></polyline>
-                        <line x1="12" y1="12" x2="20" y2="7.5"></line>
-                        <line x1="12" y1="12" x2="12" y2="21"></line>
-                        <line x1="12" y1="12" x2="4" y2="7.5"></line>
-                        <line x1="16" y1="5.25" x2="8" y2="9.75"></line>
+                    <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="12" cy="12" r="9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        <path
+                            d="M14.5 9.08333L14.3563 8.96356C13.9968 8.66403 13.5438 8.5 13.0759 8.5H10.75C9.7835 8.5 9 9.2835 9 10.25V10.25C9 11.2165 9.7835 12 10.75 12H13.25C14.2165 12 15 12.7835 15 13.75V13.75C15 14.7165 14.2165 15.5 13.25 15.5H10.412C9.8913 15.5 9.39114 15.2969 9.01782 14.934L9 14.9167"
+                            stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M12 8L12 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                        <path d="M12 17V16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
                     </svg>
                 </div>
                 <h4 class="text-xl font-medium text-gray-700">Competitive Pricing</h4>
@@ -70,14 +100,20 @@
             <div
                 class="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl"
                 data-rounded="rounded-xl" data-rounded-max="rounded-full">
-                <div class="p-3 text-white bg-blue-500 rounded-full" data-primary="blue-500"
+                <div class="p-3 text-white bg-fuchsia-500 rounded-full" data-primary="blue-500"
                      data-rounded="rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 " viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M8 9l3 3l-3 3"></path>
-                        <line x1="13" y1="15" x2="16" y2="15"></line>
-                        <rect x="3" y="4" width="18" height="16" rx="2"></rect>
+                    <svg class="w-8 h-8" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_901_2595)">
+                            <path
+                                d="M1 31C1 31 1.687 27.379 2 26C2.208 25.083 3 23 11 22C11 22 12 25 16 25C20 25 21 22 21 22C29 23 29.792 25.021 30 26C30.294 27.384 31 31 31 31M10 11C10 14.866 13 18 16 18C19 18 22 14.866 22 11M8 30V31M24 30V31M6 8H23C23 4.134 19.866 1 16 1C12.134 1 9 4.134 9 8"
+                                stroke="currentColor" stroke-width="1.8" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_901_2595">
+                                <rect width="32" height="32" fill="white"/>
+                            </clipPath>
+                        </defs>
                     </svg>
                 </div>
                 <h4 class="text-xl font-medium text-gray-700">Experienced Staff</h4>
@@ -90,16 +126,17 @@
             <div
                 class="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl"
                 data-rounded="rounded-xl" data-rounded-max="rounded-full">
-                <div class="p-3 text-white bg-blue-500 rounded-full" data-primary="blue-500"
+                <div class="p-3 text-white bg-yellow-500 rounded-full" data-primary="blue-500"
                      data-rounded="rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 " viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <line x1="9.5" y1="11" x2="9.51" y2="11"></line>
-                        <line x1="14.5" y1="11" x2="14.51" y2="11"></line>
-                        <path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path>
+                    <svg class="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
                         <path
-                            d="M7 5h1v-2h8v2h1a3 3 0 0 1 3 3v9a3 3 0 0 1 -3 3v1h-10v-1a3 3 0 0 1 -3 -3v-9a3 3 0 0 1 3 -3"></path>
+                            d="M7.88124 15.7559C8.37391 16.1826 9.02309 16.4909 9.72265 16.6928C10.4301 16.897 11.2142 17 12 17C12.7858 17 13.5699 16.897 14.2774 16.6928C14.9769 16.4909 15.6261 16.1826 16.1188 15.7559"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+                        <circle cx="9" cy="10" r="1.25" fill="currentColor" stroke="currentColor" stroke-width="0.5"
+                                stroke-linecap="round"/>
+                        <circle cx="15" cy="10" r="1.25" fill="currentColor" stroke="currentColor" stroke-width="0.5"
+                                stroke-linecap="round"/>
                     </svg>
                 </div>
                 <h4 class="text-xl font-medium text-gray-700">Customer Satisfaction</h4>
@@ -112,7 +149,7 @@
             <div
                 class="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl"
                 data-rounded="rounded-xl" data-rounded-max="rounded-full">
-                <div class="p-3 text-white bg-blue-500 rounded-full" data-primary="blue-500"
+                <div class="p-3 text-white bg-teal-500 rounded-full" data-primary="blue-500"
                      data-rounded="rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 " viewBox="0 0 24 24" stroke-width="1.5"
                          stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
