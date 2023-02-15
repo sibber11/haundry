a34731bcce100ac06e6c7a7d09c2ccd50b14a6b7
sibber11 <sibber11@gmail.com>
2023-02-15 21:24:21 +0600

    service name changed

--- database/migrations/2022_08_15_151119_create_services_table.php
+++ database/migrations/2022_08_15_151119_create_services_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -18,7 +17,7 @@ return new class extends Migration
             $table->text('name');
         });
 
-        DB::table('services')->insert([['name' => 'wash'], ['name'=>'iron'], ['name'=>'dry wash'], ['name'=>'wash iron']]);
+        DB::table('services')->insert([['name' => 'wash & fold'], ['name' => 'iron'], ['name' => 'dry clean'], ['name' => 'wash & iron']]);
 
         Schema::create('laundry_type_service', function (Blueprint $table) {
             $table->foreignId('laundry_type_id');
